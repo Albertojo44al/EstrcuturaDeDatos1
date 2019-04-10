62e18697b5492b567c3c3c92be8ac486b33e1bba
Albertojo44al <Albertojo44al@unitec.edu>
2019-04-10 11:39:38 -0600

    act

---
+++ Algoritmos/Algoritmos/Clase1Algoritmos.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int Factorial(int numero) {
+	int factorial = 1;
+	while (numero > 0) {
+		factorial = factorial * numero;
+		numero--;
+	}
+	return factorial;
+}
+bool ParImpar(int numero) {
+		if (numero % 2 == 0)
+			return true;
+	return false;
+}
+bool numeroPrimo(int numero) {
+	int r = 0;
+	for (int i = 1; i < (numero+1); i++) {
+		if (numero%i == 0)
+			r++;
+	}
+	if (r != 2)
+		return false;
+	return true;
+}
+
+
+int main() {
+	int numero = 0,opcion = 0;
+	do {
+		cout << "\n\n1. Factorial \n2.Numeros Pares \n3.Numeros Primos \n4.Salir";
+		cout << "\nIngrese una opcion: ";
+		cin >> opcion;
+		switch (opcion)
+		{
+		case 1:
+			cout << "\nIngrse un numero :";
+			cin >> numero;
+			cout << "\nEl factorial es: " << Factorial(numero);
+			break;
+		case 2:
+			cout << "\nIngrse un numero :";
+			cin >> numero;
+			ParImpar(numero) ? cout << "\nEs Par" : cout << "\nEs Impar";
+			break;
+		case 3: 
+			cout << "\nIngrse un numero :";
+			cin >> numero;
+			numeroPrimo(numero) ? cout << "\nEs Primo" : cout << "\nNo es Primo";
+			break;
+		}
+		
+	} while (opcion !=4);
+}
